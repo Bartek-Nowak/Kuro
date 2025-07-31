960c372d452b80de944fce4a6da944c968e7c891
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-07-31 16:26:39 +0200

    feat(component): add text component

---
+++ src/components/ui/text/Text.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { getTextClasses, type TextVariants } from '.'
+
+const props = defineProps<TextVariants & { as?: string, class?: string }>()
+</script>
+
+<template>
+  <component
+    :is="as || 'p'"
+    :class="getTextClasses({ variant: variant, size: size }, props.class)"
+  >
+    <slot />
+  </component>
+</template>

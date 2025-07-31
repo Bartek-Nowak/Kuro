a7d9995f8c3e8f087ab6528d42801a4795df5f0e
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-07-31 10:56:35 +0200

    refactor(callout): remove unused props to simplify component API

--- src/components/ui/callout/Callout.vue
+++ src/components/ui/callout/Callout.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-import { defineProps, useSlots } from 'vue';
 import { getCalloutClasses, type CalloutVariants } from '.';
 
 const props = defineProps<CalloutVariants & { class?: string }>();
-const slots = useSlots();
 </script>
 
 <template>
-  <div :class="getCalloutClasses(props.class, { variant: props.variant })">
-    <div v-if="slots.icon" class="pt-0.5">
+  <div :class="getCalloutClasses(props.class, { variant: variant })">
+    <div v-if="$slots.icon" class="pt-0.5">
       <slot name="icon" />
     </div>
     <div class="space-y-1">
-      <div v-if="slots.title" class="font-semibold leading-tight">
+      <div v-if="$slots.title" class="font-semibold leading-tight">
         <slot name="title" />
       </div>
       <div class="text-sm leading-relaxed">
